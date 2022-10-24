7739ebb62b7c00d717f58df5b78633468ddd129a
azzouni <91bilele@mail.com>
2022-10-24 11:43:39 +0200

    issue #3

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
@@ -24,7 +24,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 
 	private Collection<DiplomaSnippet> snippets = new HashSet<>();
 
-	public AbstractDiplomaGenerator() {
+	protected AbstractDiplomaGenerator() {
 		super();
 		
 
@@ -35,7 +35,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 	 * 
 	 * @return
 	 */
-	abstract protected Collection<DiplomaSnippet> getDiplomaSnippets();
+	protected abstract Collection<DiplomaSnippet> getDiplomaSnippets();
 
 	/*
 	 * (non-Javadoc)
